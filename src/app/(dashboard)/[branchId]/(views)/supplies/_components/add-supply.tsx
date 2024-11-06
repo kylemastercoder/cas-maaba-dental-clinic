5e8757d471a6e07a5e95d61c8b2868d61886c4bb
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-11-06 10:52:35 +0800

    remove branch in service 4

--- src/app/(dashboard)/[branchId]/(views)/supplies/_components/add-supply.tsx
+++ src/app/(dashboard)/[branchId]/(views)/supplies/_components/add-supply.tsx
@@ -1,21 +1,30 @@
 "use client";
 
 import { getAllBranches } from "@/actions/branch";
+import { getAllUnits } from "@/actions/unit";
 import SupplyForm from "@/components/modals/supply-modal";
 import { Button } from "@/components/ui/button";
-import { Branch } from "@prisma/client";
+import { Branch, Units } from "@prisma/client";
 import { IconCirclePlus } from "@tabler/icons-react";
 import React, { useEffect, useState } from "react";
 
 const AddSupply = () => {
   const [openModal, setOpenModal] = useState(false);
   const [branches, setBranches] = useState<Branch[]>([]);
+  const [units, setUnits] = useState<Units[]>([]);
   useEffect(() => {
     const fetchBranches = async () => {
       const response = await getAllBranches();
       setBranches(response?.data || []);
     };
     fetchBranches();
+  }, []);
+  useEffect(() => {
+    const fetchUnits = async () => {
+      const response = await getAllUnits();
+      setUnits(response?.data || []);
+    };
+    fetchUnits();
   }, []);
   return (
     <>
@@ -32,6 +41,7 @@ const AddSupply = () => {
 
       {openModal && (
         <SupplyForm
+          units={units}
           branches={branches}
           initialData={null}
           onClose={() => setOpenModal(false)}
--- src/app/(dashboard)/[branchId]/(views)/supplies/_components/cell-action.tsx
+++ src/app/(dashboard)/[branchId]/(views)/supplies/_components/cell-action.tsx
@@ -17,8 +17,9 @@ import AlertModal from "@/components/ui/alert-modal";
 import { SupplyColumn } from "./column";
 import { useDeleteSupply } from "@/data/supply";
 import SupplyForm from "@/components/modals/supply-modal";
-import { Branch } from "@prisma/client";
+import { Branch, Units } from "@prisma/client";
 import { getAllBranches } from "@/actions/branch";
+import { getAllUnits } from "@/actions/unit";
 
 interface CellActionProps {
   data: SupplyColumn;
@@ -27,12 +28,20 @@ interface CellActionProps {
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const [open, setOpen] = useState(false);
   const [branches, setBranches] = useState<Branch[]>([]);
+  const [units, setUnits] = useState<Units[]>([]);
   useEffect(() => {
     const fetchBranches = async () => {
       const response = await getAllBranches();
       setBranches(response?.data || []);
     };
     fetchBranches();
+  }, []);
+  useEffect(() => {
+    const fetchUnits = async () => {
+      const response = await getAllUnits();
+      setUnits(response?.data || []);
+    };
+    fetchUnits();
   }, []);
   const [formOpen, setFormOpen] = useState(false);
   const [initialData, setInitialData] = useState<SupplyColumn | null>(null);
@@ -69,6 +78,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
       {formOpen && (
         <SupplyForm
+          units={units}
           branches={branches}
           initialData={initialData}
           onClose={() => setFormOpen(false)}
