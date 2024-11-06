2e117f3c8704b5cc8f5730897cd0832f0060dce0
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-11-06 10:44:21 +0800

    remove branch in service

--- src/app/(dashboard)/[branchId]/(views)/services/_components/add-service.tsx
+++ src/app/(dashboard)/[branchId]/(views)/services/_components/add-service.tsx
@@ -1,22 +1,12 @@
 "use client";
 
-import { getAllBranches } from "@/actions/branch";
 import ServiceForm from "@/components/modals/service-modal";
 import { Button } from "@/components/ui/button";
-import { Branch } from "@prisma/client";
 import { IconCirclePlus } from "@tabler/icons-react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const AddService = () => {
   const [openModal, setOpenModal] = useState(false);
-  const [branches, setBranches] = useState<Branch[]>([])
-  useEffect(() => {
-    const fetchBranches = async () => {
-      const response = await getAllBranches();
-      setBranches(response?.data || []);
-    }
-    fetchBranches();
-  }, [])
   return (
     <>
       <Button
@@ -34,7 +24,6 @@ const AddService = () => {
         <ServiceForm
           initialData={null}
           onClose={() => setOpenModal(false)}
-          branches={branches}
         />
       )}
     </>
